8b369c34e1c5bbdbda715d1b0ed3c38d14f24e34
silvimica <mayagoloburda@gmail.com>
2023-04-15 16:28:46 +0600

    POST UI added

--- webapp.py
+++ webapp.py
@@ -17,11 +17,11 @@ st.markdown("""---""")
 st.markdown('Для начала работы, выберите таблицу. Таблица sales работает с информацией о продажах. Таблица supplies работает с информацией о закупках. Таблица Report дает доступ к получению информации о продажах и закупках.')
 st.markdown("""---""")
 
-option1 = st.selectbox('TABLE', ('Sales', 'Supply', 'Reports'))
-if option1 == 'Sales':
+option1 = st.selectbox('TABLE', ('Supply','Sales', 'Reports'))
+if option1 == 'Supply':
     st.markdown("""---""")
     st.markdown(
-        'Здесь вы можете создавать, редактировать, удалять и просмотривать записи таблицы продаж.')
+        'Здесь вы можете создавать, редактировать, удалять и просмотривать записи таблицы закупок.')
     st.markdown("""---""")
     option2 = st.selectbox('METHOD', ('GET', 'POST', 'PUT', 'DELETE'))
     if option2 == 'GET':
@@ -54,8 +54,24 @@ if option1 == 'Sales':
                 st.write("No data found for the given parameters.")
 
     elif option2 == 'POST':
-        barcode = st.text_input("BARCODE", "yyyy-MM-dd HH:mm:ss")
-        from_date = st.text_input("FROM TIME", "yyyy-MM-dd HH:mm:ss")
+        barcode = st.text_input("BARCODE", "1010101010")
+        price =st.text_input("PRICE", "0")
+        quantity = st.text_input("QUANTITY", "0")
+        supplytime = st.text_input("SUPPLY TIME", "yyyy-MM-dd HH:mm:ss")
+        submitted = st.button("Submit")
+        if submitted:
+            api_url = "http://127.0.0.1:8000/api/supplies/"
+            todo = {"barcode": barcode,
+                    "price": price, "quantity": quantity, "supplyTime": supplytime}
+            response = requests.post(api_url, json=todo)
+            data = response.json().update(todo)
+            if data:
+                st.table(data)
+            else:
+                st.write("Error occured. Try again. Possibly incorrect input form.")
+    elif option2 == 'PUT':
+        barcode = st.text_input("ID", "1010101010")
+        date_time = st.text_input("FROM TIME", "yyyy-MM-dd HH:mm:ss")
         to_date = st.text_input("TO_DATE", "yyyy-MM-dd HH:mm:ss")
         submitted = st.button("Submit")
         if submitted:
@@ -64,10 +80,10 @@ if option1 == 'Sales':
                     "toTime": to_date, "barcode": barcode}
             response = requests.post(api_url, json=todo)
             response.json()
-elif option1 == 'Supply':
+elif option1 == 'Sales':
     st.markdown("""---""")
     st.markdown(
-        'Здесь вы можете создавать, редактировать, удалять и просмотривать записи таблицы закупок.')
+        'Здесь вы можете создавать, редактировать, удалять и просмотривать записи таблицы продаж.')
     st.markdown("""---""")
     option2 = st.selectbox('METHOD', ('GET', 'POST', 'PUT', 'DELETE'))
 elif option1 == 'Reports':
