d20beb5111eb97dee402e60c1dda33fe6f4b20f4
silvimica <mayagoloburda@gmail.com>
2023-04-15 16:42:32 +0600

    Front

--- webapp.py
+++ webapp.py
@@ -86,6 +86,51 @@ elif option1 == 'Sales':
         'Здесь вы можете создавать, редактировать, удалять и просмотривать записи таблицы продаж.')
     st.markdown("""---""")
     option2 = st.selectbox('METHOD', ('GET', 'POST', 'PUT', 'DELETE'))
+    if option2 == 'GET':
+        barcode = st.text_input("BARCODE", "4870204391510", max_chars=13)
+
+        default_startdate_str = "2022/01/01"
+        default_startdate = datetime.strptime(
+            default_startdate_str, '%Y/%m/%d')
+        from_date = st.date_input(
+            "Enter a date and time", value=default_startdate, key=1)
+        
+        default_enddate_str = "2022/12/31"
+        default_enddate = datetime.strptime(default_enddate_str, '%Y/%m/%d')
+        to_date = st.date_input("Enter a date and time",
+                                value=default_enddate, key=2)
+        
+        submitted = st.button("Submit")
+        if submitted:
+            api_url = "http://127.0.0.1:8000/api/supplies/"
+            todo = {"fromTime": from_date.isoformat(),
+                    "toTime": to_date.isoformat(), 
+                    "barcode": barcode}
+
+            response = requests.get(api_url, json=todo)
+            data = response.json()
+
+            if data:
+                st.table(data)
+            else:
+                st.write("No data found for the given parameters.")
+
+    elif option2 == 'POST':
+        barcode = st.text_input("BARCODE", "1010101010")
+        price =st.text_input("PRICE", "0")
+        quantity = st.text_input("QUANTITY", "0")
+        supplytime = st.text_input("SUPPLY TIME", "yyyy-MM-dd HH:mm:ss")
+        submitted = st.button("Submit")
+        if submitted:
+            api_url = "http://127.0.0.1:8000/api/supplies/"
+            todo = {"barcode": barcode,
+                    "price": price, "quantity": quantity, "supplyTime": supplytime}
+            response = requests.post(api_url, json=todo)
+            data = response.json().update(todo)
+            if data:
+                st.table(data)
+            else:
+                st.write("Error occured. Try again. Possibly incorrect input form.")
 elif option1 == 'Reports':
     st.markdown("""---""")
     st.markdown(
