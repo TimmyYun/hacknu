7e40c3bbe673fcb5e587006a29b14c1584a38956
Tamerlan-K <tamerlan.khussainov@nu.edu.kz>
2023-04-16 22:15:01 +0600

    a

--- umag_zapis/umag_zapis/settings.py
+++ umag_zapis/umag_zapis/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "umag_zapis.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "hacknu",
+        "NAME": "Umag",
         "USER": "postgres",
         "PASSWORD": "adminadmin",
         "HOST": "localhost",
